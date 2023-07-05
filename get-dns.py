55dfe2cc6e4ffe31ab97fa274ab10da92fcf955c
rmajcher <russell.majcher@pluto.tv>
2023-07-05 09:18:45 -0600

    no longer required to know the exact number of records you are searching for

--- get-dns.py
+++ get-dns.py
@@ -61,28 +61,49 @@ def assumerole(account_id):
     return route53_client
 
 def main():
+    # If record is the root record example: plutopreprod.tv
     if args.record in list(hosted_zones_and_accounts.keys()):
         domain = args.record
+    # All other records besides root records
     else:
         domain = args.record.split(".", 1)[1]
     route53_client = assumerole(hosted_zones_and_accounts[domain]['account_id'])
 
-    if args.recordtype:
-        response = route53_client.list_resource_record_sets(
-            HostedZoneId = hosted_zones_and_accounts[domain]['zone_id'],
-            StartRecordName = args.record,
-            StartRecordType = args.recordtype,
-            MaxItems = args.numberofentries
-        )['ResourceRecordSets']
+    # If number of entries is specified
+    if args.numberofentries != '1':
+        # If specified a specific starting record type example: CNAME
+        if args.recordtype:
+            response = route53_client.list_resource_record_sets(
+                HostedZoneId = hosted_zones_and_accounts[domain]['zone_id'],
+                StartRecordName = args.record,
+                StartRecordType = args.recordtype,
+                MaxItems = args.numberofentries
+            )['ResourceRecordSets']
+        # All other searches will be generic for starting record type
+        else:
+            response = route53_client.list_resource_record_sets(
+                HostedZoneId = hosted_zones_and_accounts[domain]['zone_id'],
+                StartRecordName = args.record,
+                MaxItems = args.numberofentries
+            )['ResourceRecordSets']
+        # Print json response for record + number of entries
+        pretty_json = json.dumps(response, indent=4)
+        print(pretty_json)
+    # Will specifically find the correct entries and put them in a json variable
     else:
+        rhm = []
         response = route53_client.list_resource_record_sets(
             HostedZoneId = hosted_zones_and_accounts[domain]['zone_id'],
             StartRecordName = args.record,
-            MaxItems = args.numberofentries
+            MaxItems = '20'
         )['ResourceRecordSets']
 
-    pretty_json = json.dumps(response, indent=4)
-    print(pretty_json)
+        for i in range(0, len(response)):
+            if f"{args.record}." == response[i]['Name']:
+                if response[i]['Type'] in ['A', 'CNAME']:
+                    rhm.append(response[i])
+        pretty_json = json.dumps(rhm, indent=4)
+        print(pretty_json)
 
 if __name__ == '__main__':
     main()
